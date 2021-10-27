c20bc5b54e9b4c45de0cffa3a9d5066508afcead
Avery Nortonsmith <averynortonsmith@fastmail.com>
2021-10-27 18:36:09 -0400

    export 'v' function

--- src/villeray.js
+++ src/villeray.js
@@ -2,6 +2,7 @@ export { getParams, matchPath, route } from "./route.js";
 export { mount } from "./mount.js";
 export { refresh } from "./refresh.js";
 export { serialize } from "./serialize.js";
+export { v } from "./vnode.js";
 
 // route: path, params
 // aftercreate
